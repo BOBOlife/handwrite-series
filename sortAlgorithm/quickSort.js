5e897dbc646685112b6a28371fc351c07634e0e9
wangbo <bbw1996@qq.com>
2021-02-28 20:40:34 +0800

    快速排序

--- sortAlgorithm/quickSort.js
+++ sortAlgorithm/quickSort.js
@@ -1,14 +1,16 @@
 const quickSort = arr => {
 	if(arr.length <= 1){return arr}
-	cosnt halfIndex = Math.floor(arr.length/2)
-	cosnt randomNum = arr.splice(halfIndex,1)[0]
-	let left  = [],rigth=[]
+	const pivotIndex = Math.floor(arr.length/2)
+	const pivot = arr.splice(pivotIndex,1)[0]
+	let left  = [],right=[]
 	for(let i=0;i<arr.length;i++){
-		if(arr[i]< randomNum){
+		if(arr[i]< pivot){
 			left.push(arr[i])
 		}else{
 			right.push(arr[i])
 		}
 	}
-	return quickSort(left).concat([randomNum],quickSort(right))
+	return quickSort(left).concat([pivot],quickSort(right))
 }
+
+console.log(quickSort([4,2,5,1,7,3]))
